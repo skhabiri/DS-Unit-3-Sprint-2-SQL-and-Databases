2ed95a26b9d4b8299b92c60898bd388fbdf3e118
skhabiri <shahnam.khabiri@gmail.com>
2020-08-10 16:24:07 -0700

    updated file

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -2,21 +2,31 @@ import sqlite3
 
 conn = sqlite3.connect("rpg_db.sqlite3")
 curs = conn.cursor()
-curs.execute(query)
-curs.fetchall()
 
 
-SELECT COUNT(character_id)
-FROM charactercreator_character
+def execute_query(cursor, query):
+    cursor.execute(query)
+    return cursor.fetchall()
+
 
 if __name__ == '__main__':
+
     Total_char = """
     SELECT COUNT(character_id)
     FROM charactercreator_character
     """
-    curs.execute(Total_char)
-    results = curs.fetchall()
+    result = execute_query(curs, Total_char)
     print("Total number of characters:")
-    print(results)
+    print(result)
 
-    
+    char_class = ["mage", "thief", "cleric", "fighter"]
+    results = []
+
+    for idx, item in enumerate(char_class):
+        query = """
+        SELECT COUNT(character_ptr_id)
+        FROM charactercreator_""" + item
+        results.append(execute_query(curs, query))
+
+        print(f"{item} characters:")
+        print(results[idx][0][0])
