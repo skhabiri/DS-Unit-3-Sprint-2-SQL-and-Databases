79ade397530a44501b645081dbbbbfd133c29fb8
skhabiri <shahnam.khabiri@gmail.com>
2020-08-10 18:42:06 -0700

    updated the rpg_quiries file

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
     print("Total number of characters:")
     print(result)
 
+    # Number of characters in each class
     char_class = ["mage", "thief", "cleric", "fighter"]
     results = []
 
@@ -30,3 +31,68 @@ if __name__ == '__main__':
 
         print(f"{item} characters:")
         print(results[idx][0][0])
+
+    # How many total Items?
+    query = """
+    SELECT COUNT(item_id)
+    FROM armory_item
+    """
+    print("Total items:")
+    print(execute_query(curs, query))
+
+    #How many of the Items are weapons? How many are not?
+    query = """
+    SELECT COUNT(item_ptr_id)
+    FROM armory_weapon
+    """
+    print("Total Weapons:")
+    print(execute_query(curs, query))
+
+    #How many of the Items are not weapons?
+    query="""
+    SELECT COUNT(*) FROM
+    (SELECT item_id  FROM armory_item
+    WHERE item_id NOT IN  (SELECT item_ptr_id FROM armory_weapon));
+    """
+    print("Non Weapon items:")
+    print(execute_query(curs, query))
+
+    #How many of the Items are not weapons? alternative way
+    query="""
+    SELECT COUNT(item_id) FROM
+    (SELECT item_id  FROM armory_item
+    LEFT JOIN armory_weapon
+    ON item_id=item_ptr_id
+    WHERE item_ptr_id IS NULL)
+    """
+    print("Non Weapon items: alternative")
+    print(execute_query(curs, query))
+
+    #How many Items does each character have? (Return first 20 rows)
+    query="""
+    SELECT character_id, COUNT(item_id)
+    FROM charactercreator_character_inventory
+    GROUP BY 1 ORDER BY 2 DESC LIMIT 20
+    """
+    print("ITEMS PER CHARACTER:")
+    print(execute_query(curs, query))
+
+    #How many Weapons does each character have? (Return first 20 rows)
+    query="""
+    SELECT character_id, COUNT(item_id) FROM
+    (SELECT character_id, item_id, item_ptr_id FROM charactercreator_character_inventory, armory_weapon
+    WHERE item_id=item_ptr_id)
+    GROUP BY character_id ORDER BY 2 DESC LIMIT 20
+    """
+    print("WEAPONS PER CHARACTER:")
+    print(execute_query(curs, query))
+
+    # On average, how many Items does each Character have?
+    query="""
+    SELECT AVG(item_count) FROM
+    (SELECT character_id, COUNT(item_id) AS item_count
+    FROM charactercreator_character_inventory
+    GROUP BY 1 ORDER BY 2 DESC)
+    """
+    print("Average number of items per character:")
+    print(execute_query(curs, query))
